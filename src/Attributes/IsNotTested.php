b29aaf29ea48bd4baef7d65c96b328f544be735b
BadPixxel <eshop.bpaquier@gmail.com>
2024-04-21 15:17:36 +0200

    ADD - No Tested Field Flag

---
+++ src/Attributes/IsNotTested.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Splash\Metadata\Attributes;
+
+use Attribute;
+use Splash\Metadata\Interfaces\FieldMetadataConfigurator;
+use Splash\Metadata\Mapping\FieldMetadata;
+
+/**
+ * Mark Field as Not Tested
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class IsNotTested implements FieldMetadataConfigurator
+{
+    public function __construct(
+        protected bool $enabled = true,
+    ) {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function configure(FieldMetadata $metadata): void
+    {
+        if ($this->enabled) {
+            $metadata
+                ->setIsNotTested(true)
+            ;
+        }
+    }
+}
